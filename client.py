f22b79ab0312e8650b1d6a0ad90e936ccd334feb
Biffyclyro <joao-senna1@hotmail.com>
2020-12-14 18:14:22 -0300

    versão apresentação

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 import socket
 import time
 
+# metodo responsável por enviar a mensagem tcp
 def send_msg_tcp(msg):
     with socket.socket() as s:
         s.connect(('localhost', 8090))
         s.send(bytes(msg))
-        print(s.recv(1024))
+        print(s.recv(1024).decode('utf-8'))
 
+# método responsaǘel por enviar mensagem udp
 def send_msg_udp(msg):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.sendto( bytes(msg), ('localhost', 8080))
 
 
-
+#classe responsável por simular colheitadeira
 class Colheiradeira:
     max_lng = 10
     max_lat = 10
@@ -44,4 +46,5 @@ class Colheiradeira:
 c = Colheiradeira(0, 0)
 
 while c.lat < c.max_lat:
-   send_msg_udp( c.move())
+   send_msg_tcp( c.move())
+   #send_msg_udp( c.move())
--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import socket
 from threading import Thread
 
-
+#método que espera request na porta 8090
 def tcp_listener():
     with socket.create_server(('localhost', 8090)) as ss:
         while True:
+            #accept espera por uma conexão
             conn, addr = ss.accept()
             with conn:
                 print('conectado com', addr)
+                #mensagem é recebida pela conexão estabelecida
                 print( tuple(conn.recv(1024)) )
+                conn.send(b'movimento recebido')
     
-
+#método que espera request na porta 8080
 def udp_listener():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ss:
         ss.bind(('localhost', 8080))
         while True:
+            #mensagem é recebia sem ser necessário estabelecer uma conexão
             msg, addr = ss.recvfrom(1024)
             print(f'{tuple(msg)} recebido de {addr}')
 
